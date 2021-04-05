6799797d4b25005841406f8ef9cd3b4b12fc183b
daniel <dhrc1997@gmail.com>
2021-04-05 08:17:47 -0300

    Initial tests - using Junit4

---
+++ src/test/Tests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Tests {
+
+    @Test
+    public void politicaTest() {
+        assertEquals(new Object(), new Object());
+    }
+}
